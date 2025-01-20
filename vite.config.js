d3bf34e33bc2287cc1c49684c4a1ee75a988289a
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-20 15:19:14 +0500

    Fix issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['typed.js', 'react-scroll', 'clsx'],
+      external: ['typed.js', 'react-scroll', 'clsx', 'tailwind-merge'],
     },
   },
 });
