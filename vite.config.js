4e6567bf7ce2be78fe84832f2c54364457e5a40d
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-20 14:23:00 +0500

    new change in despendencies

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['typed.js'],
+      external: ['typed.js', 'react-scroll'],
     },
   },
 });
