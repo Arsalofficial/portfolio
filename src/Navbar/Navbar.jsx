8f023d36e7f40f13c5d4bd9f421545be9ac5b867
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-21 10:03:18 +0500

    new changes

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import AOS from 'aos'; // Import AOS library
 import 'aos/dist/aos.css'; // Import AOS CSS
-import { Link, animateScroll as scroll } from './node_modules/react-scroll'; // Import scroll
+import { Link, animateScroll as scroll } from 'react-scroll'; // Import scroll
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
