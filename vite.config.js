65cf9e0ddea42aaf7648c465ae545aa84135ab03
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-21 10:24:23 +0500

    add new changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-scroll': require.resolve('react-scroll'), // Resolve react-scroll correctly
+    },
+  },
   build: {
     rollupOptions: {
-      external: ['typed.js', 'react-scroll', 'clsx', 'tailwind-merge'],
+      external: ['typed.js', 'react-scroll', 'clsx', 'tailwind-merge'], // External dependencies
     },
   },
 });
