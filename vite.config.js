8f8393d45d5ab562bea3df764b3eb6f41a78978f
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-21 10:01:58 +0500

    new changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['typed.js',  'clsx', 'tailwind-merge'],
+      external: ['typed.js', 'react-scroll', 'clsx', 'tailwind-merge'],
     },
   },
 });
